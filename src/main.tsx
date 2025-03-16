61d1668546617d5af4bf93a2a93b87bcd8ec3edc
Emanuele <emanuelegirardello1@gmail.com>
2025-03-16 18:28:28 +0100

    Configurato Firebase +  Autenticazione

--- src/main.tsx
+++ src/main.tsx
@@ -14,7 +14,7 @@ createRoot(document.getElementById('root')!).render(
         <Route path = "/" element = {<HomePage/>}/>    
       </Routes>
       <Routes>
-        <Route path = "/login" element = {<Prova/>}/>    
+        <Route path = "/login" element = {<Login/>}/>    
       </Routes>
     </Router>
   
