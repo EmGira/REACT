3d73a6f5a5969082063e49cad500aacb53918f2f
Edoardo <edopoltro@gmail.com>
2025-04-28 17:29:18 +0200

    aggiornamento user

--- src/App.tsx
+++ src/App.tsx
@@ -5,18 +5,15 @@ import Home from './components/home/Home';
 import SignIn from './components/signup_login/SignIn';
 import SignUp from './components/signup_login/SignUp';
 import CalendarComponent from './components/calendario/Calendario';
-import ProtectedRoute from './components/signup_login/ProtectedRoute';
 import {useAuth} from './components/contexts/AuthContext'
 import {BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom'
 import User from './components/user/User';
-import Profilo from './components/user/profilo/Profilo';
-import Registro from './components/user/registro/Registro';
-import Piano from './components/user/piano/Piano';
 
 
 
 
-function Impaginazione({ children } ){
+
+function Impaginazione({ children }: any){
     return (
         <>  
             <Header></Header>
@@ -28,33 +25,33 @@ function Impaginazione({ children } ){
 
 function App(){
 
-    const {
-        isLoggedIn,
-        } = useAuth();          //tira fuori i valori Context del utente con useContext e li assegna alla struttura
+    const { isLoggedIn } = useAuth();  
         
     return(
         <>
             <Router>
                 <Routes>
                    
-                    <Route path = "/" element = {<SignIn/>}/>  
-                    
+                    <Route path = "/" element = {<SignIn/>}/> 
+
                     <Route path = "/signup" element = {<SignUp/>}/> 
 
+                    <Route path = "/home" element = {<Impaginazione><Home/></Impaginazione>} />
+                    
                     <Route path = "/calendar" element = {<CalendarComponent/>}/> 
-
+                    {/* 
                     //route protette
                     <Route element={<ProtectedRoute isAuthenticated = {isLoggedIn} />}>
                         <Route path = "/home" element = {<Impaginazione><Home/></Impaginazione>} />
 
-                    </Route>
+                    </Route> */}
+
+                    
+
+                    <Route path="/user/:view/:slug" element={<Impaginazione><User /></Impaginazione>}/>
 
                     <Route path = "*" element={<Navigate to="/" replace />}/>   
-                    <Route path="/user" element={<Impaginazione><User /></Impaginazione>}>
-                        <Route path="profilo" element={<Profilo />} />
-                        <Route path="registro" element={<Registro />} />
-                        <Route path="piano" element={<Piano />} />
-                    </Route>
+
                 </Routes>
             </Router>
         </>
--- src/components/user/User.tsx
+++ src/components/user/User.tsx
@@ -1,7 +1,10 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./User.css"
-import { Outlet, useNavigate, useParams } from 'react-router-dom';
+import { useNavigate, useParams } from 'react-router-dom';
 import Profilo from "./profilo/Profilo";
+import { FirebaseService } from '../../services/FirebaseService';
+import Registro from "./registro/Registro";
+import Piano from "./piano/piano";
 
 
   export const user:any = {
@@ -20,37 +23,71 @@ import Profilo from "./profilo/Profilo";
 
 
 function User() {
-    const navigate = useNavigate();
-    const { name } = useParams(); 
 
+    const [currentUser, setCurrentUser] = useState<any>(null);
+    const [currentSlug, setCurrentSlug] = useState('');
+    const [currentView, setCurrentView] = useState('');
+
+    const { view, slug } = useParams();
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        
+
+        if(view == null || slug == null){
+            navigate('/');
+            return;
+        }
+
+        setCurrentSlug(slug);
+        setCurrentView(view);
+
+        if(['piano','profilo','registro'].includes(view)){
+            FirebaseService.getAllUsers().then((users: any) => {
+                const user = users.find((u: any) => u.id == slug.split('-')[0]) || null;
+                if(user == null){
+                    navigate('/');
+                    return;
+                }
+                else
+                    setCurrentUser(user); 
+            });
+        }
+        else{
+            navigate('/');
+            return; 
+        }
+
+    },[view, slug, navigate]);
+    
     const image = ["/src/assets/user/manIcon.svg", "/src/assets/user/girlIcon.svg"];
 
     const icon = (genere : string) => {
         return user.genere === "M" ? image[0] : image[1];
     }   
 
-    const[editColorButton, setEditColorButton] = useState(0);
-
-    const button_menu = ["profilo", "registro", "piano"];
+    // const[editColorButton, setEditColorButton] = useState(0);
 
 
-return(
-    <div className="body_user">
-        <div className="box_user">
-            <div className="cerchio_icon">
-                <img src={icon(user.genere)} className="icon"/>
+    return(
+        <div className="body_user">
+            <div className="box_user">
+                <div className="cerchio_icon">
+                    <img src={icon(currentUser.genere)} className="icon"/>
+                </div>
+                <h1 className="h1_name_user">{currentUser.nome} {currentUser.cognome}</h1>
+    
+                <div className="div_button_nav">
+                    <button className="menu_button" onClick={() =>  navigate(`/user/profilo/` + currentSlug)}>profilo</button>
+                    <button className="menu_button" onClick={() =>  navigate(`/user/registro/` + currentSlug)}>registro</button>
+                    <button className="menu_button" onClick={() =>  navigate(`/user/piano/` + currentSlug)}>piano</button>
+                </div>
             </div>
-            <h1 className="h1_name_user">{user.nome} {user.cognome}</h1>
-
-            <div className="div_button_nav">
-                {button_menu.map((name) =>(
-                    <button key={name} className="menu_button" onClick={() =>  navigate(`/user/${name}`)}>{name}</button>
-                ))}
-            </div>
+            {currentView == 'registro' && <Registro></Registro>}
+            {currentView == 'profilo' && <Profilo></Profilo>}
+            {currentView == 'piano' && <Piano></Piano>}
         </div>
-        <Outlet />
-    </div>
-)
+    )
 
 }
 
